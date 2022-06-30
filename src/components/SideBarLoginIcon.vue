b88b93766535c69cf3901765527e5fc55479088c
igormuba <igorm-uba@hotmail.com>
2022-06-30 12:17:04 -0300

    change login button if loggged in

--- src/components/SideBarLoginIcon.vue
+++ src/components/SideBarLoginIcon.vue
@@ -6,15 +6,23 @@
 
   <div class="flex items-center justify-start">
     <div class="flex-shrink-0" @click="toggleLoginModal">
-      <img class="rounded-full w-16" :src="Login" alt="+" />
+      <img
+        v-if="accountStore.account.authenticated"
+        class="rounded-full"
+        :src="`https://images.hive.blog/u/${accountStore.account.name}/avatar/small`"
+        alt="@"
+      />
+      <img v-else class="rounded-full w-16" :src="Login" alt="+" />
     </div>
   </div>
 </template>
 <script setup>
+import { useAccountStore } from "../stores/account";
 import Login from "../assets/images/icons/login.svg";
+const accountStore = useAccountStore();
 
 const props = defineProps({});
-let loginModalOpen = ref(true);
+let loginModalOpen = ref(accountStore.account.authenticated);
 const toggleLoginModal = () => {
   loginModalOpen.value = !loginModalOpen.value;
 };
