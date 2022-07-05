9cd443073ddcb08de22b9c7647e204e356eaebe3
igormuba <igorm-uba@hotmail.com>
2022-07-05 18:25:50 -0300

    redirect when logout

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -4,10 +4,7 @@
       <div class="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
         <div class="flex items-center"></div>
         <div class="ml-10 space-x-4">
-          <a
-            href="#"
-            @click="accountStore.signOut()"
-            class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium hover:bg-indigo-50"
+          <a href="#" @click="logout()" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium hover:bg-indigo-50"
             >Logout</a
           >
         </div>
@@ -23,5 +20,11 @@
 <script setup>
 import { useAccountStore } from "../stores/account";
 const accountStore = useAccountStore();
+const router = useRouter();
+
+function logout() {
+  accountStore.signOut();
+  router.push("/landing");
+}
 //will be used when click on user's own profile to logout, change settings, etc
 </script>
