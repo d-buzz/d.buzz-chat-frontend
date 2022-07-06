ceadfbff1f1e12cb27ca6b8a59dd8a56ff2d9557
igormuba <igorm-uba@hotmail.com>
2022-07-06 09:52:05 -0300

    private chat text input and send button

--- src/components/PrivateChat.vue
+++ src/components/PrivateChat.vue
@@ -1,10 +1,11 @@
 <template>
   <input
-    class="fixed bottom-0 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    class="fixed bottom-0 shadow appearance-none border rounded-full w-[calc(100%-4rem)] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
     id="Message"
     type="text"
     placeholder="Message"
   />
+  <button class="fixed bottom-0 right-0 x-16 bg-primary text-white font-bold py-2 px-4 rounded-full">Send</button>
 </template>
 <script setup>
 //will be used when click on someone else's profile to chat 1-on-1
