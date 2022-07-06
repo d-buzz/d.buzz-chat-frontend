dc97d32b37628a4267c599a96e8560df318f905f
igormuba <igorm-uba@hotmail.com>
2022-07-06 09:27:57 -0300

    margin left on router-view to not overlap sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <SideBar></SideBar>
-  <router-view></router-view>
+  <div class="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg overflow-x-scroll">
+    <SideBar></SideBar>
+  </div>
+  <div class="ml-16">
+    <router-view></router-view>
+  </div>
 </template>
 <script setup></script>
