4a29c0bca6d48cab7ba8c06e76575299e9156747
igormuba <igorm-uba@hotmail.com>
2022-07-06 09:30:15 -0300

    message box on private chat

--- src/components/PrivateChat.vue
+++ src/components/PrivateChat.vue
@@ -1,4 +1,11 @@
-<template>Private Chat</template>
+<template>
+  <input
+    class="fixed bottom-0 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    id="Message"
+    type="text"
+    placeholder="Message"
+  />
+</template>
 <script setup>
 //will be used when click on someone else's profile to chat 1-on-1
 </script>
