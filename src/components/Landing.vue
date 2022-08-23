2f3381552a4b670e660d6f389b386f25b1017f7b
Yuuki <mirafungames@gmail.com>
2022-08-23 23:02:34 +0200

    landing page template wip

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,8 +1,48 @@
 <template>
-    <h1>landing page</h1>
-    
-    <a class="btn" href="/controlpanel">Open</a>
+<div>
+  <section class="mb-40">
+    <nav class="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+      <div class="px-6 w-full flex flex-wrap items-center justify-between">
+        <div class="flex items-center">
+          <a class="navbar-brand text-green-700" href="#!">
+            <span class="oi oi-chat"></span>
+          </a>
+        </div>
+        <div class="grow items-center" id="navbarSupportedContentY">
+          <ul class="navbar-nav mr-auto flex flex-row">
+            <li class="nav-item">
+              <a class="nav-link block pr-2 px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+            </li>
+          </ul>
+        </div>
+        <div class="flex items-center lg:ml-auto">
+          <button type="button" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-green-600 font-medium text-xs leading-tight uppercase rounded hover:text-green-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Login</button>
+          <button type="button" class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Sign up for free</button>
+        </div>
+      </div>
+    </nav>
+
+    <div class="px-6 py-12 md:px-12 bg-gray-50 text-gray-800 text-center lg:text-left">
+      <div class="container mx-auto xl:px-32">
+        <div class="grid lg:grid-cols-2 gap-12 flex items-center">
+          <div class="mt-12 lg:mt-0">
+            <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">A place <br /><span class="text-green-600">for your community</span></h1>
+            <a class="inline-block px-7 py-3 mr-2 bg-green-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/controlpanel" role="button">Open App</a>
+            <!--<a class="inline-block px-7 py-3 bg-transparent text-green-600 font-medium text-sm leading-snug uppercase rounded hover:text-green-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Learn more</a>-->
+          </div>
+          <div class="mb-12 lg:mb-0">
+             <img
+                    class="w-full rounded-full"
+                    :src="`https://images.hive.blog/u/peakd/avatar/large`"
+                    alt="@"
+                    />
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
 </template>
 <script setup>
-console.log("landing page");
 </script>
