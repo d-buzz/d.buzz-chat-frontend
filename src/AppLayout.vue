52d2926d1e333b233914495e0a36c08ba1495529
Yuuki <mirafungames@gmail.com>
2022-10-12 01:32:19 +0200

    responsive streambar

--- src/AppLayout.vue
+++ src/AppLayout.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="flex min-h-full h-screen appbg2">
     <div class="h-screen flex flex-col appbg0 text-secondary shadow-lg">
+        <button class="w-full avCommunity md:hidden border-r-1" style="padding-left: 1px;"
+             @click="toggleStreambar()"><span class="oi oi-menu" style="font-size:30px;"></span></button>
         <SideBar></SideBar>
     </div>
-    <div class="h-screen flex flex-col appbg1 text-secondary shadow-lg mr-3">
+    <div class="streambar h-screen flex flex-col appbg1 text-secondary shadow-lg" ref="streamBar">
         <StreamBar :key="getKey(route.path)"></StreamBar>
     </div>
-    <div class="grow">
+    <div class="ml-3 grow">
         <router-view :key="getKey(route.path)"></router-view>
     </div>
   </div>
 </template>
 <script setup>
+const streamBar = ref(null);
 const route = useRoute();
 function getKey(path) {
     return path + stlib.Utils.nextId();
 }
+function toggleStreambar() {
+    var value = streamBar.value.dataset.show;
+    streamBar.value.dataset.show = !("true" === value);
+}
 </script>
+<style scoped>
+@media (max-width: 767px) {
+    .streambar {
+        display: none;
+        position: fixed;
+        z-index: 11;
+        left: 63px;
+    }
+    .streambar[data-show="true"] {
+        display: block;
+    }
+}
+</style>
