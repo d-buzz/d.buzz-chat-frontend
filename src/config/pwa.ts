9531d333e37698797cb6d2fda4596bcc77c2d946
Yuuki <mirafungames@gmail.com>
2023-05-26 15:12:37 +0200

    delete config/pwa.ts

--- src/config/pwa.ts
+++
@@ -1,8 +0,0 @@
-import { ViteSetupModule } from "../config/types/ViteSetupModule";
-
-export const priority = 0;
-
-export const install: ViteSetupModule = async ({ app }) => {
-  const { registerSW } = await import("virtual:pwa-register");
-  registerSW({ immediate: true });
-};
